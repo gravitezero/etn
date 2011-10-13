26cc05f359426dccccc01957378ee3788c589665
Etienne Brodu <etienne.brodu@insa-lyon.fr>
2011-10-13 15:39:51 +0200

    theme

---
+++ themes/etn-theme/README.md
@@ -0,0 +1,7 @@
+Etn theme Nesta theme
+=====================
+
+etn-theme is a theme for Nesta, a [Ruby CMS](nesta), designed by
+<insert your name here>.
+
+[nesta]: http://effectif.com/nesta
---
+++ themes/etn-theme/app.rb
@@ -0,0 +1,19 @@
+# Use the app.rb file to load Ruby code, modify or extend the models, or
+# do whatever else you fancy when the theme is loaded.
+
+module Nesta
+  class App
+    # Uncomment the Rack::Static line below if your theme has assets
+    # (i.e images or JavaScript).
+    #
+    # Put your assets in themes/etn-theme/public/etn-theme.
+    #
+    # use Rack::Static, :urls => ["/etn-theme"], :root => "themes/etn-theme/public"
+
+    helpers do
+      # Add new helpers here.
+    end
+
+    # Add new routes here.
+  end
+end
